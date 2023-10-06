a31557b42cface16778db4af933306d507524f13
kaw393939 <kwilliam@njit.edu>
2023-10-06 14:20:38 -0400

    added utf8 test closes #14

--- tests/homepage.spec.js
+++ tests/homepage.spec.js
@@ -11,3 +11,13 @@ test('check that "express works" text is present', async ({ page }) => {
 
 });
 
+test('check that UTF-8 meta tag is present', async ({ page }) => {
+  //Arrange: Go to the site homepage
+  await page.goto('/');
+
+  //Act: Get the content attribute of the meta charset tag
+  const metaCharset = await page.$eval('meta[charset]', (meta) => meta.getAttribute('charset'));
+
+  //Assert: Check if the charset is set to UTF-8
+  await expect(metaCharset).toBe('utf-8');
+});
